3486df7619fd19ae53ed21e55b98550b5def3620
znlgis <znlgis@foxmail.com>
2017-05-21 23:03:31 +0800

    45037	dth_cp	2017/5/18 14:09:26	模块路由 45036	dth_cp	2017/5/18 14:09:00	内存数据库 45035	dth_cp	2017/5/17 15:44:57	修改日期序列化方式 45033	dth_cp	2017/5/16 17:54:52	内存数据库，完善导入导出功能 45031	dth_cp	2017/5/15 16:33:23	完善异常日志记录 45025	dth_cp	2017/5/9 10:52:04	增加内存数据库 45023	dth_cp	2017/5/8 19:29:10	改进异步方法调用 45021	dth_cp	2017/5/8 10:02:06	增加模块路由功能 45020	dth_cp	2017/5/6 17:58:24	增加事务执行的异常捕获 45019	dth_cp	2017/5/5 11:03:49	改进实体类索引器赋值，可用于大批量文本数据导入 45017	dth_cp	2017/5/2 16:09:42	EntityQuery 增加QueryEnumerable 方法，用于大批量数据读取 45016	dth_cp	2017/5/2 15:35:35	为EntityQuery的QuickInsert 方法增加测试代码 45014	dth_cp	2017/4/28 18:47:53	实体类 setProperty增加 DbType 参数重载，允许用户指定字符串属性对应的字段类型 45007	dth_cp	2017/4/27 18:21:48	对于SqlServer,MySQL，QuickInsert 将采用合并数据并且事务插入方式 45002	dth_cp	2017/4/27 15:33:04	解决OQL多实体类查询动态排序的一个问题 45000	dth_cp	2017/4/26 12:06:02	修改生成唯一顺序ID算法 44990	dth_cp	2017/4/24 9:35:16	完善查询异常对象类，可通过InnerException查看原始异常 44989	dth_cp	2017/4/23 11:40:59	增加签名 44987	dth_cp	2017/4/21 15:28:08	完善MySQL参数类型和字段类型名称映射 44984	dth_cp	2017/4/20 11:39:03	修正事件发布问题 44982	dth_cp	2017/4/18 20:13:25	支持事件服务对象的生命周期管理 44979	dth_cp	2017/4/17 12:02:09	更改版本号 44978	dth_cp	2017/4/16 23:24:21	消息服务框架增加分布式事件支持 44977	dth_cp	2017/4/16 20:27:27	测试服务类 44976	dth_cp	2017/4/16 9:41:39	增加发布分布式事件 44973	dth_cp	2017/4/14 23:14:30	解决OQL使用SUM,MAX等聚合函数同时分页可能发生的错误问题 44972	dth_cp	2017/4/12 16:15:14	修改Oracle NClob类型判断

---
+++ MessageServiceFramework/Test/Service/AlarmClock.cs
@@ -0,0 +1,81 @@
+﻿using PWMIS.EnterpriseFramework.Service.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ServiceSample
+{
+    public class AlarmClockService : IService
+    {
+        System.Timers.Timer timer;
+        DateTime AlarmTime;
+        IServiceContext CurrentContext;
+        int publishCount;
+
+        public event EventHandler Alarming;
+
+        public AlarmClockService()
+        {
+            timer = new System.Timers.Timer();
+            timer.Interval = 10000;
+            timer.Elapsed += timer_Elapsed;
+        }
+
+        void timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            if (e.SignalTime >= this.AlarmTime)
+            {
+                if (Alarming != null)
+                    Alarming(this, new EventArgs());
+
+                CurrentContext.PublishData(DateTime.Now); //e.SignalTime
+                publishCount++;
+                Console.WriteLine("AlarmClockService Publish Count:{0}",publishCount);
+            }
+            if (publishCount > 10)
+            {
+                timer.Stop();
+                Console.WriteLine("AlarmClockService Timer Stoped. ");
+            }
+        }
+
+       
+        public ServiceEventSource SetAlarmTime(DateTime targetTime)
+        {
+            return new ServiceEventSource(timer, 2, () => {
+                //要初始化执行的代码或者方法
+                publishCount = 0;
+                this.AlarmTime = targetTime;
+                timer.Start();
+                //如果上面的代码是一个执行时间比较长的方法，但又不知道何时执行完成，
+                //并且不想等待超时回收服务对象，而是在执行完成后立即回收服务对象，可以调用下面的代码：
+                //CurrentContext.PublishEventSource.DeActive();
+                //注意：调用DeActive 方法后将会停止事件推送，所以请注意此方法调用的时机。
+
+                //下面代码仅做测试，查看服务事件源对象的活动生命周期
+                int life = CurrentContext.PublishEventSource.ActiveLife;
+
+                //如果上面执行的是一个执行时间比较长的方法，并且有返回值，想将返回值也推送给订阅端，可以再次执行CurrentContext.PublishData
+                CurrentContext.PublishData(DateTime.Now);
+            });
+        }
+
+
+        public void CompleteRequest(IServiceContext context)
+        {
+
+        }
+
+        public bool ProcessRequest(IServiceContext context)
+        {
+            this.CurrentContext = context;
+            return true;
+        }
+
+        public bool IsUnSubscribe
+        {
+            get { return false; }
+        }
+    }
+}
